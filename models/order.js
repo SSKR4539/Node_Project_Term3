9084990fcea41683936dcbf6556ea5511cfff4ed
sumanth <sumanthkumarrajus@gmail.com>
2021-04-12 17:45:07 -0400

    Update order.js

--- models/order.js
+++ models/order.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
 
-  id: { type: Number, required: true },
   name: { type: String, required: true },
   price: { type: String, required: true },
   description: { type: String, required: true },
